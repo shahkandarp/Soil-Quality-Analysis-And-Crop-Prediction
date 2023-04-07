8ea12fbadcf17819c9bf5afa8b97764da090de81
Krupal Bavishi <krupalbavishi004@gmail.com>
2023-04-07 16:14:48 +0530

    error page updated

--- frontend/src/components/Error.js
+++ frontend/src/components/Error.js
@@ -5,7 +5,7 @@ import "../styles/Error.css"
 const Error = () => {
   return (
     <div className='error-div'><br />
-      404 : ` Page not found ` <br /><br />
+      404 : ` Page not found !` <br /><br />
       <NavLink to="/"><button className='err-btn'>Home page</button></NavLink>
     </div>
   )
