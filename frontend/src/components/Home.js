a5dde087a33b88416ddbadd2f96ef3d3a3243956
shahkandarp <102229699+shahkandarp@users.noreply.github.com>
2023-05-12 00:42:29 +0530

    Chnages in home page

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
     <div className='home'>
       <img src={home} alt="" className='homeImg'/>
       <p className='h-p1'>Welcome to our <br /> crop prediction website</p>
-      <p className='h-p2'>This website provides information on crop prediction using machine learning algorithms.</p>
+      <p className='h-p2'>This website provides information on crop prediction using machine learning algorithm and IoT.</p>
       <NavLink className="h-nav" to="/login"><button className='h-button'>Get Started</button></NavLink>
       
     </div>
